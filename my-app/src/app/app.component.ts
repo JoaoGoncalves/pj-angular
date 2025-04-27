dff74093dcca6166ec8cebce39538be5076d91b5
Joao Goncalves <joao@joaogoncalves.net>
2025-04-28 00:18:42 +0100

    key logger observable - exemplo de operador

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { CopyrightDirective } from './copyright.directive';
 import { APP_SETTINGS, appSettings } from './app.settings';
-import { from, Observable, of } from 'rxjs';
+import { filter, from, interval, map, Observable, of } from 'rxjs';
+import { KeyLoggerComponent } from "./key-logger/key-logger.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductListComponent, CopyrightDirective],
+  imports: [RouterOutlet, ProductListComponent, CopyrightDirective, KeyLoggerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   providers: [{provide: APP_SETTINGS, useValue: appSettings}]
@@ -22,12 +23,15 @@ export class AppComponent {
   })
 
   constructor(){
-    this.title$.subscribe(this.setTitle)
+    this.title$.subscribe(this.setTitle);
+
+    
   }
 
   private setTitle = () => {
     const timestamp = new Date();
     this.title = `${this.settings.title} <br> ${timestamp}`;
+
   }
 
  /*  constructor(){
@@ -46,8 +50,31 @@ export class AppComponent {
     let nomes$ = from(nomes);
     nomes$.subscribe( val => console.log(val));
 
+    const numbers$ = interval(1000);
+    numbers$.subscribe( n => console.log('Numero: ', n ));
 
+    console.log("---------- operators ---------")
+    
+    let cervejas = [
+      {nome:'Sagres', pais:'Portugal', preco: 1.90},
+      {nome:'Stella', pais:'Belgica', preco: 3.90},
+      {nome:'Trappe', pais:'Belgica', preco: 4.90},
+      {nome:'Guiness', pais:'Irlanda', preco: 2.90},
+      {nome:'Brahma', pais:'Brasil', preco: 1.50},
+    ];
 
+    let cervejas$ = from(cervejas);
+
+    cervejas$.pipe(
+      filter(cerveja => cerveja.preco > 2.5),
+      map( cerveja => `Cerveja: ${cerveja.nome} - €${cerveja.preco}`)
+    ).subscribe({
+      next: cerveja => console.log(cerveja),
+      error: err => console.log(err),
+      complete: ()=> console.log('Acabaram os Chopinhos, muda para o Vinho...')
+    });
+
+    console.log("-------------------")
 
   } */
 
