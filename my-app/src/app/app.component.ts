9e9888ff2563e364fa4f8318d380ea9117af66cb
Joao Goncalves <joao@joaogoncalves.net>
2025-05-06 10:55:54 +0100

    meio sessao 3

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { CopyrightDirective } from './copyright.directive';
 import { APP_SETTINGS, appSettings } from './app.settings';
-import { from, of } from 'rxjs';
+import { from, interval, Observable, of } from 'rxjs';
+import { KeyLoggerComponent } from "./key-logger/key-logger.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductListComponent, CopyrightDirective],
+  imports: [RouterOutlet, ProductListComponent, CopyrightDirective, KeyLoggerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   providers: [{provide: APP_SETTINGS, useValue: appSettings}]
@@ -17,10 +18,33 @@ export class AppComponent {
   title = 'Ola Universo';
   settings = inject(APP_SETTINGS);
 
+  title$ = new Observable( observer => {
+    setInterval( () => observer.next(undefined), 1000)
+  } )
+
   constructor(){
+    this.title$.subscribe( this.setTitle );
+    this.testeOperators();
+  }
+  /* setTitle() {
+    const timestamp = new Date();
+    this.title = `${this.settings.title} <br> ${timestamp}`;
+  } */
+  
+  private setTitle = () => {
+    const timestamp = new Date();
+    this.title = `${this.settings.title} <br> ${timestamp}`;
+  }
+
+
+  testeOperators(){
+    console.log('----------- Operators ------------');
+  }
+
+  /* constructor(){
 
     console.log('------------- of() ------------------');
-    //of(1 ,2 ,'joao', true, 6, 9).subscribe( val => console.log(val));
+    of(1 ,2 ,'joao', true, 6, 9).subscribe( val => console.log(val));
     of(1 ,2 ,'joao', true, 6, 9).subscribe({
       next: val => console.log(val),
       error: err => console.error(err),
@@ -35,9 +59,8 @@ export class AppComponent {
 
     nomes$.subscribe( val => console.log(val))
 
+    //interval(2000).subscribe( v => console.log(v))
 
-
-
-  }
+  } */
 
 }
